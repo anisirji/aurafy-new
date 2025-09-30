16127e085856cec424ea553be3f3b4568cfd5960
anisirji <www.allinoneforyouhere@gmail.com>
2025-09-30 22:05:38 +0530

    chore: configure dev script, fix hydration, update configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import { ToastProvider } from "@/components/toast-provider";
 
 export const metadata: Metadata = {
   title: "Portfolio Website Builder",
-  description: "Create your professional portfolio in minutes with our easy-to-use builder. Select a style, choose colors, add your details, and you're ready to go!",
+  description:
+    "Create your professional portfolio in minutes with our easy-to-use builder. Select a style, choose colors, add your details, and you're ready to go!",
 };
 
 export default function RootLayout({
@@ -13,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <ToastProvider> {children}</ToastProvider>
       </body>
     </html>
